c2b00064a1258844b048d223a7ecf30aefadc40b
Sneha Murali <snehamurali.0704@gmail.com>
2025-07-01 10:20:19 +0530

    Assignment

--- list_compar.py
+++
@@ -1,9 +0,0 @@
-N = int(input('Enter number of elements of the array: '))
-try:
-    numbers = [float(num) for num in input().split()]
-    # Normal execution continues
-    print(numbers)
-except ValueError as err:
-    print('You may have entered an invalid float number')
-numbers.sort()
-print(numbers)
